90571e4fbc52b8500081620239cc8d5275e777cb
Ahren-with-an-h <agroesch@gmail.com>
2020-09-10 19:00:41 -0400

    complete redux integration

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,44 @@
 import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
 import "./App.css";
 import Scroll from "../components/Scroll";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import ErrorBoundry from "../components/ErrorBoundry";
+import { setSearchField, requestRobots } from "../actions";
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  onRequestRobots: () => dispatch(requestRobots()),
+});
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
-
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
+    this.props.onRequestRobots()
   }
-
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
 
   render() {
-    const { robots, searchfield } = this.state;
+    const { searchField, onSearchChange,robots,isPending } = this.props;
     const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    if (!robots.length) {
+    if (isPending) {
       return <h1 className="f1">Loading</h1>;
-    } else
+    } else {
       return (
         <Fragment>
           <header className="flex justify-center">
             <h1 className="f1 mh4">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
+            <SearchBox searchChange={onSearchChange} />
           </header>
           <main className="tc">
             <Scroll>
@@ -47,7 +49,8 @@ class App extends Component {
           </main>
         </Fragment>
       );
+    }
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
